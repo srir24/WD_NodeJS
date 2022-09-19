b5cbe9b2434e1a32ae564b3249da6429f2e169bb
srir24 <ravula.srikanh@gmail.com>
2022-09-19 12:48:03 +0530

    Example - ESLint

---
+++ L1_Pub/index.js
@@ -0,0 +1,23 @@
+/* ESLint extension for Visual Studio Code.
+
+ESLint - Quickly ananlyze then quckly find and fix problems
+standard tool used for identifying and reportinf coding issues in any kind of JS code
+This make code more consistent
+It is intellignet enough to suggest fixes for issues
+*/
+
+/* -- Orginal version which shows 14 errors
+//npm install -g eslint
+h1="hello, Srikanth"
+fun1=()=>{
+    console.log(h1)
+}
+
+fun1()  */
+
+const h1 = 'Hello Node.js!'
+const fun1 = () => {
+  console.log(h1)
+}
+
+fun1()
---
+++ L1_Pub/repl.js
@@ -0,0 +1,38 @@
+/*const printLog=()=>{
+console.log("Hi Srikanth")
+console.log("Welcome to NodeJS")
+}
+
+printLog()*/
+
+/*
+//Autocompletion - Node.JS REPL
+//Tabs are used for autocompletion
+str="Hello, Srikanth"
+str.toLowerCase()
+*/
+
+/*
+// Underscore Charcter - Used to refer last value
+`My name is ${_}`
+//Replaces with hello, srikanth
+*/
+
+/*.exit
+//Exit from node
+.exit
+*/
+/*.save
+.save <filename>
+//All session nformrion is going to be stored in <filename>
+//u can access it using cat <filename>
+*/
+
+/* .load
+.load <filename>
+//Load all session information from <filename> to current session
+*/
+
+//Every expression in REPL is persistent
+//will be stored in .node_repl_history file in home directory
+
